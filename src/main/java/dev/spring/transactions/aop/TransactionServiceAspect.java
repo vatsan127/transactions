ae5634107e10950c523a2408e2c4872bd82c97c8
vatsan127 <vatsan127@gmail.com>
2024-12-30 23:01:18 +0530

    commit for changing pointcut

--- src/main/java/dev/spring/transactions/aop/TransactionServiceAspect.java
+++ src/main/java/dev/spring/transactions/aop/TransactionServiceAspect.java
@@ -6,38 +6,36 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.Date;
 
 @Aspect
 @Slf4j
 @Component
-public class TransactionAspect {
+public class TransactionServiceAspect {
 
     private TransactionConfig config;
 
-    public TransactionAspect(TransactionConfig config) {
+    public TransactionServiceAspect(TransactionConfig config) {
         this.config = config;
     }
 
-    @Pointcut("execution(* dev.spring.transactions.service.*.*(..))")
-    public void allServiceMethods() {
-    }
+    private static final String POINTCUT_EXEC_FOR_ALL_SERVICE_METHODS = "execution(* dev.spring.transactions.service.*.*(..))";
 
-    @Before("allServiceMethods()")
+/*    @Pointcut("execution(* dev.spring.transactions.service.*.*(..))")
+    public void allServiceMethods() {
+    }*/
+
+    @Before(POINTCUT_EXEC_FOR_ALL_SERVICE_METHODS)
     public void logBefore(JoinPoint joinPoint) {
         logMethodDetails(true, joinPoint, null);
     }
 
-    @AfterReturning(pointcut = "allServiceMethods()", returning = "value")
+    @AfterReturning(pointcut = POINTCUT_EXEC_FOR_ALL_SERVICE_METHODS, returning = "value")
     public void logReturn(JoinPoint joinPoint, Object value) {
         logMethodDetails(false, joinPoint, value);
     }
 
-    @AfterThrowing(pointcut = "allServiceMethods()", throwing = "ex")
+    @AfterThrowing(pointcut = POINTCUT_EXEC_FOR_ALL_SERVICE_METHODS, throwing = "ex")
     public void logException(Exception ex) {
         log.error("Exception message: {}", ex.getMessage());
     }
@@ -45,14 +43,14 @@ public class TransactionAspect {
     private void logMethodDetails(boolean isBefore, JoinPoint joinPoint, Object returnValue) {
         if (!config.isDebugModeEnabled()) return;
         StringBuilder message = new StringBuilder();
-        message.append(String.format("Method: '%s'. ", LocalDateTime.now(), joinPoint.getSignature().getName()));
+        message.append(String.format("Method: '%s'", joinPoint.getSignature().getName()));
 
         if (isBefore) {
             Object[] args = joinPoint.getArgs();
-            if (args.length > 0) message.append("Params: ").append(Arrays.toString(args));
+            if (args.length > 0) message.append(" :: Params: ").append(Arrays.toString(args));
         }
 
-        if (returnValue != null) message.append(", Returned value: ").append(returnValue);
+        if (returnValue != null) message.append(" :: Returned value: ").append(returnValue);
         log.info(message.toString());
     }
 }
