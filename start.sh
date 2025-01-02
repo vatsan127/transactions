fe8aff0c09715d921809f000e20f967b4d2e03cd
Srivatsan <110617159+vatsan127@users.noreply.github.com>
2025-01-02 19:20:30 +0530

    commit for adding dependency dir in target folder

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 mvn clean install -DskipTests
+mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
 docker build -t transaction:$TIMESTAMP .
 docker-compose up --build-arg TIMESTAMP=$TIMESTAMP --file docker-compose.yml -d
