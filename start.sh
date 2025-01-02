143e0e5cccde47241413621ebf5f0190d50ee7bf
vatsan127 <vatsan127@gmail.com>
2025-01-02 20:05:47 +0530

    commit for adding docker changs

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+
 mvn clean install -DskipTests
-mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
-docker builder prune -f
+mkdir -p target/dependency
+cd target/dependency && jar -xf ../*.jar && cd -
+
 docker build -t transaction:$TIMESTAMP .
-docker-compose up --build-arg TIMESTAMP=$TIMESTAMP --force-recreate
+docker-compose up --build --force-recreate --build-arg TIMESTAMP=$TIMESTAMP
