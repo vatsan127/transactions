e5888ddb2ed1ae352291ca78d6faa8680619795f
Srivatsan <110617159+vatsan127@users.noreply.github.com>
2025-01-03 19:44:51 +0530

    commit for adding the maria db config

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@ mvn clean install -DskipTests
 mkdir -p target/dependency
 cd target/dependency && jar -xf ../*.jar && cd -
 
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+echo "Timestamp: $(date +"%Y-%m-%d %H:%M:%S")"
+
 docker tag transactions:latest transactions:$(date +"%Y%m%d%H%M%S")
 docker build -t transactions:latest .
-docker-compose up
+#docker-compose up --file docker-compose-mysql.yml
+docker-compose up --file docker-compose-mariadb.yml
