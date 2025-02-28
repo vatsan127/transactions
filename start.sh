c38e5e8ff43521e48d3292c7c628bb0ba3527108
vatsan127 <vatsan127@gmail.com>
2025-02-28 22:40:06 +0530

    commit for updatign start.sh ....

--- start.sh
+++ start.sh
@@ -2,11 +2,10 @@
 
 mvn clean install -DskipTests
 mkdir -p target/dependency
-cd target/dependency && jar -xf ../../*.jar && cd -
+cd target/dependency && jar -xf ../*.jar && cd -
 
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
-echo "Timestamp: $(TIMESTAMP)"
+echo "Docker Tag: $(TIMESTAMP)"
 
 docker tag transactions:latest transactions:$TIMESTAMP
 docker build -t transactions:latest .
-docker-compose up --file compose.yml
