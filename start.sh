3ef840b04d6d227dc662b5b744b165a7a8b32ed5
vatsan127 <vatsan127@gmail.com>
2025-02-28 22:50:33 +0530

    commit for updatign start.sh ....

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ mkdir -p target/dependency
 cd target/dependency && jar -xf ../*.jar && cd -
 
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
-echo "Docker Tag: $(TIMESTAMP)"
+echo "Old Tag: $TIMESTAMP"
 
 docker tag transactions:latest transactions:$TIMESTAMP
 docker build -t transactions:latest .
